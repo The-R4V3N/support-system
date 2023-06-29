675bd1d5f2ed775c2ee5ca50acaa9e8aa51bf2c4
Oliver Joisten <o.joisten@live.se>
2023-06-29 22:47:36 +0200

    📝 chore(userModel.js): add user model 🎉 feat(userModel.js): create user model with name, email, password, and isAdmin fields The user model is added to the backend. It includes fields for name, email, password, and isAdmin. The name field is required and cannot exceed 100 characters. The email field is required, unique, and cannot exceed 100 characters. The password field is required, must be at least 6 characters long, and must contain at least one special character. The isAdmin field is a boolean field with a default value of false.

---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'Please enter your name'],
+			trim: true,
+			maxLength: [100, 'Name cannot exceed 100 characters'],
+		},
+		email: {
+			type: String,
+			required: [true, 'Please enter your email'],
+			unique: true,
+			trim: true,
+			maxLength: [100, 'email cannot exceed 100 characters'],
+		},
+		password: {
+			type: String,
+			required: [true, 'Please enter your password'],
+			minLength: [6, 'Password must be at least 6 characters'],
+			match: [
+				/^(?=.*[!@#$%^&*])/,
+				'Password must contain at least one special character (!@#$%^&*)',
+			],
+			select: false,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
