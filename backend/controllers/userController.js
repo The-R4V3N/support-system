9cff3b15222de6489ba18d849d3116d098b117db
Oliver Joisten <o.joisten@live.se>
2023-06-29 22:08:06 +0200

    🔧 fix(userController.js): add validation checks for name, email, and password in registerUser function 🔧 fix(server.js): add error handling middleware
    The registerUser function in userController.js now includes validation checks for name, email, and password. If any of these fields are missing, a 400 status code is returned with an error message.
    
    The server.js file now includes an error handling middleware called errorHandler. This middleware handles any errors that occur during the request-response cycle and sends an appropriate response with the error message and stack trace (in development mode).

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,26 @@
+// @desc   Register user
+// @route  POST /api/users
+// @access Public
 const registerUser = (req, res) => {
+	const { name, email, password } = req.body
+
+	// Validation checks for name, email, and password
+	if (!name || !email || !password) {
+		res.status(400)
+		throw new Error('Please enter all fields')
+	}
+
 	res.send('Register Route')
 }
 
+// @desc   Login user
+// @route  POST /api/users/login
+// @access Public
 const loginUser = (req, res) => {
 	res.send('Login Route')
 }
 
+// Export functions
 module.exports = {
 	registerUser,
 	loginUser,
