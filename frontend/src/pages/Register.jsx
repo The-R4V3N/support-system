3804f114045f84e4a0f78beff2cd89691ab59788
Oliver Joisten <o.joisten@live.se>
2023-06-30 02:05:13 +0200

    🎨 style(App.js): import and use ToastContainer component from react-toastify library The ToastContainer component from the react-toastify library is imported and added to the App component. This component is responsible for displaying toast notifications in the application.
    🎨 style(Register.jsx): add form for user registration
    🔧 chore(Register.jsx): refactor form to use useState hook for form data and add form validation
    The Register component now includes a form for user registration. The form uses the useState hook to manage form data and form validation. The form includes fields for name, email, password, and password confirmation. The form also includes form validation to check if the password and password confirmation match. If they do not match, an error toast is displayed.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,97 @@
-
+import { useState } from 'react'
+import {toast} from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
 
 function Register() {
-  return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  )
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		passwordConfirm: '',
+	})
+
+	const { name, email, password, passwordConfirm } = formData
+
+	const onChange = (e) =>
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (password !== passwordConfirm) {
+            toast.error('Passwords do not match')
+        } 
+    }
+
+	return (
+		<>
+			<section className='heading'>
+				<h1>
+					<FaUser /> Register
+				</h1>
+				<p>Please create an account</p>
+			</section>
+
+			<section className='form'>
+				<form onSubmit={onSubmit}>
+					<div className='form-group'>
+						<input
+							type='text'
+							className='form-control'
+							id='name'
+							name='name'
+							value={name}
+                            required
+							placeholder='Enter your name'
+							onChange={onChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='text'
+							className='form-control'
+							id='email'
+							name='email'
+							value={email}
+                            required
+							placeholder='Enter your email'
+							onChange={onChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							className='form-control'
+							id='password'
+							name='password'
+							value={password}
+                            required
+							placeholder='Enter a password'
+							onChange={onChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							className='form-control'
+							id='passwordConfirm'
+							name='passwordConfirm'
+							value={passwordConfirm}
+                            required
+							placeholder='Confirm password'
+							onChange={onChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<button className='btn btn-block'>Submit</button>
+					</div>
+				</form>
+			</section>
+		</>
+	)
 }
 
 export default Register
