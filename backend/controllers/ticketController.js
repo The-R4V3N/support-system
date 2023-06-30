b08da01637bd015e41fe9bb6178fa0904b792aff
Oliver Joisten <o.joisten@live.se>
2023-06-30 23:17:49 +0200

    🐛 fix(ticketController.js): add mongoose import for ObjectId.isValid() workaround ✨ feat(ticketController.js): add getTicket route and logic The ticketController.js file has been updated to include the import of the mongoose library for the ObjectId.isValid() workaround. Additionally, a new route and logic for getting a specific ticket has been added. The route is a private route that requires authentication. The logic checks if the user exists and if the ticket exists. It also checks if the user is authorized to view the ticket. If all conditions are met, the ticket is returned in the response.
    🔥 feat(ticketController.js): add deleteTicket and updateTicket functions
    🔒 fix(ticketController.js): add authorization checks for deleteTicket and updateTicket functions
    The deleteTicket function allows users to delete a ticket by its ID. It performs authorization checks to ensure that the user is authorized to delete the ticket. If the user is not found or the ticket is not found, appropriate error responses are sent. The ticket is deleted using the deleteOne method.
    
    The updateTicket function allows users to update a ticket by its ID. It also performs authorization checks to ensure that the user is authorized to update the ticket. If the user is not found or the ticket is not found, appropriate error responses are sent. The ticket is updated using the findByIdAndUpdate method with the updated data provided in the request body. The updated ticket is then returned in the response.
    
    🔀 refactor(ticketsRoutes.js): reorganize and add CRUD routes for tickets
    The ticketsRoutes.js file has been refactored to reorganize the code and add CRUD routes for tickets. The getTicket, updateTicket, and deleteTicket functions have been added to the ticketController module and are now imported and used in the ticketsRoutes module. This allows for retrieving, updating, and deleting individual tickets by their ID. The routes are protected and require authentication using the protect middleware.

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler")
+const mongoose = require("mongoose") // For ObjectId.isValid() workaround
 
 const User = require("../models/userModel")
 
@@ -19,6 +20,44 @@ const getTickets = asyncHandler(async (req, res) => {
   const tickets = await Ticket.find({ user: req.user._id })
 
   res.status(200).json(tickets)
+})
+
+// @desc   Get user ticket
+// @route  GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user id from token
+  const user = await User.findById(req.user._id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error("User not found")
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error("Ticket not found")
+  }
+
+  // Workaround for the commendted out code below
+  if (!mongoose.Types.ObjectId.isValid(req.user._id)) {
+    res.status(401)
+    throw new Error("Invalid user ID")
+  }
+
+  if (!ticket.user.equals(req.user._id)) {
+    res.status(401)
+    throw new Error("Not authorized to view this ticket")
+  }
+
+  //   if (ticket.user.toString() !== req.user._id) {
+  //     res.status(401)
+  //     throw new Error("Not authorized to view this ticket")
+  //   }
+
+  res.status(200).json(ticket)
 })
 
 // @desc   Get new ticket
@@ -50,7 +89,94 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket)
 })
 
+// @desc   Delete ticket
+// @route  DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get user id from token
+  const user = await User.findById(req.user._id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error("User not found")
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error("Ticket not found")
+  }
+
+  // Workaround for the commendted out code below
+  if (!mongoose.Types.ObjectId.isValid(req.user._id)) {
+    res.status(401)
+    throw new Error("Invalid user ID")
+  }
+
+  if (!ticket.user.equals(req.user._id)) {
+    res.status(401)
+    throw new Error("Not authorized to view this ticket")
+  }
+
+  //   if (ticket.user.toString() !== req.user._id) {
+  //     res.status(401)
+  //     throw new Error("Not authorized to view this ticket")
+  //   }
+
+  await ticket.deleteOne()
+
+  res.status(200).json({ success: true, message: "Ticket deleted" })
+})
+
+// @desc   Update ticket
+// @route  PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user id from token
+  const user = await User.findById(req.user._id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error("User not found")
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error("Ticket not found")
+  }
+
+  // Workaround for the commendted out code below
+  if (!mongoose.Types.ObjectId.isValid(req.user._id)) {
+    res.status(401)
+    throw new Error("Invalid user ID")
+  }
+
+  if (!ticket.user.equals(req.user._id)) {
+    res.status(401)
+    throw new Error("Not authorized to view this ticket")
+  }
+
+  //   if (ticket.user.toString() !== req.user._id) {
+  //     res.status(401)
+  //     throw new Error("Not authorized to view this ticket")
+  //   }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
+
+  res.status(200).json(updatedTicket)
+})
+
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  updateTicket,
+  deleteTicket,
 }
