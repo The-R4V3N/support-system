d34d314f15fb23b576bcbb20f7e8166cb80823d5
Oliver Joisten <o.joisten@live.se>
2023-06-30 15:39:22 +0200

    🔧 fix(Header.jsx): import missing dependencies and update logout functionality 🔧 fix(authService.js): add logout function The Header component was missing the import statements for `useNavigate`, `useSelector`, `useDispatch` from `react-router-dom` and `react-redux` respectively. The logout functionality in the Header component has been updated to dispatch the `logout` and `reset` actions from the `authSlice` and navigate to the home page after logging out. The `authService` module now exports a `logout` function to remove the user data from local storage.
    🔒 chore(authSlice.js): add logout functionality to authSlice
    🔒 is used to indicate a chore or maintenance task.
    
    The logout functionality has been added to the authSlice in the authSlice.js file. The logout action is now dispatched using the createAsyncThunk function. When the logout action is fulfilled, the user state is set to null, effectively logging out the user.

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -6,15 +6,19 @@ const API_URL = '/api/users'
 const register = async (userData) => {
 	const response = await axios.post(API_URL, userData)
 
-    if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+	if (response.data) {
+		localStorage.setItem('user', JSON.stringify(response.data))
+	}
 
-    return response.data
+	return response.data
 }
 
+// Logout user
+const logout = () => localStorage.removeItem('user')
+
 const authService = {
-    register
+	register,
+	logout,
 }
 
 export default authService
