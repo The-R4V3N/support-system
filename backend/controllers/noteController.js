7d4ce68c32c01a8faf413d028b01cc6fe9dfcde2
Oliver Joisten <o.joisten@live.se>
2023-07-02 02:02:46 +0200

    📝 chore(noteController.js): add noteController.js file 🔒 feat(noteController.js): implement getNotes and addNote functions The noteController.js file is added to the backend/controllers directory. The getNotes function retrieves notes for a specific ticket, while the addNote function creates a new note for a ticket. These functions are used to handle the corresponding routes in the application.
    📦 feat(noteModel.js): add note model
    📦 feat(noteRoutes.js): add note routes
    🔀 chore(ticketsRoutes.js): re-route into note router
    The note model has been added to the backend/models directory. It defines the schema for a note, including the user, ticket, text, isStaff, and staffId fields. The note routes have also been added to the backend/routes directory. The routes include a GET route to retrieve notes and a POST route to add a new note. The tickets routes have been updated to re-route requests for notes to the note router. This allows for better organization and separation of concerns.

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,78 @@
+const asyncHandler = require("express-async-handler")
+const mongoose = require("mongoose") // For ObjectId.isValid() workaround
+
+const User = require("../models/userModel")
+const Note = require("../models/noteModel")
+const Ticket = require("../models/ticketModel")
+
+// @desc   Get notes for a ticket
+// @route  GET /api/tickets/:ticketId/notes
+// @access Private
+const getNotes = asyncHandler(async (req, res) => {
+  // Get user id from token
+  const user = await User.findById(req.user._id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error("User not found")
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId)
+
+  // Workaround for the commendted out code below
+  if (!mongoose.Types.ObjectId.isValid(req.user._id)) {
+    res.status(401)
+    throw new Error("Invalid user ID")
+  }
+
+  if (!ticket.user.equals(req.user._id)) {
+    res.status(401)
+    throw new Error("Not authorized to view this ticket")
+  }
+
+  //   if (ticket.user.toString() !== req.user._id) {
+  //     res.status(401)
+  //     throw new Error("Not authorized to view this ticket")
+  //   }
+
+  const notes = await Note.find({ ticket: req.params.ticketId })
+
+  res.status(200).json(notes)
+})
+
+// @desc    Create ticket note
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const addNote = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.ticketId)
+
+  // Workaround for the commendted out code below
+  if (!mongoose.Types.ObjectId.isValid(req.user._id)) {
+    res.status(401)
+    throw new Error("Invalid user ID")
+  }
+
+  if (!ticket.user.equals(req.user._id)) {
+    res.status(401)
+    throw new Error("Not authorized to view this ticket")
+  }
+
+  //   if (ticket.user.toString() !== req.user.id) {
+  //     res.status(401)
+  //     throw new Error("User not authorized")
+  //   }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id,
+  })
+
+  res.status(200).json(note)
+})
+
+module.exports = {
+  getNotes,
+  addNote,
+}
